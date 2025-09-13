f1377a76deaec6396f64836363bec7d5a94429f0
FloerSoloer <floer.solo.03@gmail.com>
2025-09-13 10:06:22 +0800

    replace sensitive values with .env

--- autostart.sh
+++ autostart.sh
@@ -3,6 +3,6 @@
 awd="${0:A:h}"
 aservice='sftpgo.service'
 aservice_dir="${XDG_CONFIG_HOME:-"${HOME}/.config"}/systemd/user"
-sed 's;${APATH};'"${awd}"/docker-compose.yml';' "${awd}/${aservice}.example" > "${aservice_dir}/${aservice}"
-systemctl --user add-wants autostart.target "${aservice}"
+sed 's;${AWD};'"${awd}"';' "${awd}/${aservice}.example" > "${aservice_dir}/${aservice}"
+systemctl --user add-wants xdg-desktop-autostart.target "${aservice}"
 systemctl --user start "${aservice}"
